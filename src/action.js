74550aa30113661f023592bd6ee47e312054e15f
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2025-02-15 10:32:44 +0900

    Fix: Removes reference to non iterable element

--- src/action.js
+++ src/action.js
@@ -267,7 +267,7 @@ function addToSubmissions(params) {
     submissions,
   } = params;
 
-  for (const submission of response.data.data.submissionList.submissions) {
+  for (const submission of response.data.data.submissionList) {
     submissionTimestamp = Number(submission.timestamp);
     if (submissionTimestamp <= lastTimestamp) {
       return false;
